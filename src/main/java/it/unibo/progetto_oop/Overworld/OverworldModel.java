dc226e14f63f477dbe9b8989c9f5cf28bf9e7f5b
Kelly <kelly.applebee@studio.unibo.it>
2025-04-11 15:04:05 +0200

    Neighbours metodo, setters e getters Non è ancora stato testato ma questa sarà il template che seguirò

--- src/main/java/it/unibo/progetto_oop/Overworld/OverworldModel.java
+++ src/main/java/it/unibo/progetto_oop/Overworld/OverworldModel.java
@@ -1,9 +1,44 @@
 package it.unibo.progetto_oop.Overworld;
 
+import java.util.List;
+
 import it.unibo.progetto_oop.combattimento.Position;
 
 public class OverworldModel {
     
-    
+    private Position player;
+    private Position tempPlayer;
+    private List<Position> enemies;
+    private List<Position> walls; 
+
+    public OverworldModel(Position player, List<Position> enemies, List<Position> walls){
+        this.player = player;
+        this.enemies = enemies;
+        this.walls = walls;
+    }
+
+    private Position setPlayerAttempt(int directionx, int directiony){
+
+        this.tempPlayer = new Position(this.player.x() + directionx, this.player.y() + directiony);
+        return this.walls.stream().anyMatch(pos -> pos.equals(new Position(pos.x() + directionx, pos.y() + directiony))) ? this.player : this.tempPlayer;
+    }
+
+
+    // --- getter methods ---
+
+    public Position getPlayer(){
+        return this.player;
+    }
+
+    public List<Position> getEnemies(){
+        return this.enemies;
+    }
+
+    // --- setter methods ---
+
+    public Position setPlayer(int directionx, int directiony){
+        this.player = setPlayerAttempt(directionx, directiony);
+        return this.player;
+    }
 
 }
