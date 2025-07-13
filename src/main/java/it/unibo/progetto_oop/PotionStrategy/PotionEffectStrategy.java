023ec2532b6366f167daea22f573a43a81fb4030
k3lly200413 <81436078+k3lly200413@users.noreply.github.com>
2025-07-14 01:19:32 +0200

    Delete src/main/java/it/unibo/progetto_oop/PotionStrategy directory

--- src/main/java/it/unibo/progetto_oop/PotionStrategy/PotionEffectStrategy.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.progetto_oop.PotionStrategy;
-
-import it.unibo.progetto_oop.google.CombatModel;
-
-public interface PotionEffectStrategy {
-    void apllyEffect(CombatModel user);
-}
